5f5e9eec097101c2b315acacef37985d63ff53d8
Mehmet TUFEKCI <mtufekci1446@hotmail.com>
2023-10-04 23:27:14 +0300

    Cache removing and cache grouping implemtation added

--- Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
+++ Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
@@ -24,6 +24,8 @@ public class GetListBrandQuery:IRequest<GetListResponse<GetListBrandListItemDto>
 
     public TimeSpan? SlidingExpiration { get; }
 
+    public string? CacheGroupKey => "GetBrands";
+
     public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, GetListResponse<GetListBrandListItemDto>>
     {
         private readonly IBrandRepository _brandRepository;
